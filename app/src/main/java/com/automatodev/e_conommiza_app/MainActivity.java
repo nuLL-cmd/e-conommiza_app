57eb246cac60246930d356b47e37d52c4acf2bcb
Marco Aurelio <programadormoderador@hotmail.com>
2021-02-17 12:07:02 -0200

    Initial commit - crete new project

---
+++ app/src/main/java/com/automatodev/e_conommiza_app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.automatodev.e_conommiza_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
