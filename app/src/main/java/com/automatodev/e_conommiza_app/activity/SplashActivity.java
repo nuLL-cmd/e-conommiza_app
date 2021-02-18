6cae66b7bb526b704f324799e1972b2c19f227c1
Marco Aurelio <programadormoderador@hotmail.com>
2021-02-18 00:42:03 -0200

    Implmentation SplashActivity finish

--- app/src/main/java/com/automatodev/e_conommiza_app/activity/SplashActivity.java
+++ app/src/main/java/com/automatodev/e_conommiza_app/activity/SplashActivity.java
@@ -2,15 +2,61 @@ package com.automatodev.e_conommiza_app.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.automatodev.e_conommiza_app.R;
+import com.automatodev.e_conommiza_app.databinding.ActivitySplashBinding;
 
+@SuppressLint("SetTextI18n")
 public class SplashActivity extends AppCompatActivity {
+    ActivitySplashBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        View viewSplash = binding.getRoot();
+        setContentView(viewSplash);
+
+
+        showVersion();
+        splash();
+    }
+
+
+
+    public void showVersion(){
+        try{
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), 0);
+            if (info != null){
+                binding.txtVersaoSplash.setText("Versão : "+ info.versionName);
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void splash(){
+
+        new Thread(){
+            @Override
+            public void run(){
+                try{
+                    sleep(3000);
+                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                    finish();
+                }catch(InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
     }
 }
