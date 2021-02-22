37ef3ee99c6ec40e43fc5d54929dccb2a9c28272
Marco Aurelio <programadormoderador@hotmail.com>
2021-02-22 14:15:46 -0300

    Make LoginActivity and RegisterActivity activityes

---
+++ app/src/main/java/com/automatodev/e_conommiza_app/activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.automatodev.e_conommiza_app.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.automatodev.e_conommiza_app.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
