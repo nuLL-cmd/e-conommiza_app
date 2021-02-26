7b6c6e9a6a450025e6a42eee7861061956ec99b8
Marco Aurelio <programadormoderador@hotmail.com>
2021-02-26 13:24:35 -0300

    Design layout_dialog_about.xml and method for initialization dialog

--- app/src/main/java/com/automatodev/e_conommiza_app/ProfileActivity.java
+++ app/src/main/java/com/automatodev/e_conommiza_app/ProfileActivity.java
@@ -1,17 +1,26 @@
 package com.automatodev.e_conommiza_app;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.automatodev.e_conommiza_app.databinding.ActivityProfileBinding;
+import com.automatodev.e_conommiza_app.databinding.LayoutDialogAboutBinding;
 
 public class ProfileActivity extends AppCompatActivity {
-
+    private ActivityProfileBinding binding;
     public static boolean status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        binding = ActivityProfileBinding.inflate(getLayoutInflater());
+        View viewBinding = binding.getRoot();
+        setContentView(viewBinding);
     }
 
 
@@ -25,6 +34,16 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         status = false;
+    }
+
+
+    public void about(View view){
+        LayoutDialogAboutBinding layoutBinding  = DataBindingUtil.inflate(getLayoutInflater().from(this),R.layout.layout_dialog_about,binding.relativeDaddyProfile,false);
+        AlertDialog dialog = new AlertDialog.Builder(this).create();
+        dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        dialog.setView(layoutBinding.getRoot());
+        dialog.show();
+        layoutBinding.btnCloseDialogAbout.setOnClickListener(view1 -> dialog.dismiss());
     }
 
 
