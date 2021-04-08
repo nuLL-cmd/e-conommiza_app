9dadcb11f0df8f0972110e1ce2dde700bee3dc9c
Marco Aurelio <programadormoderador@hotmail.com>
2021-04-07 21:43:24 -0300

    Init module - add registers.

---
+++ app/src/main/java/com/automatodev/e_conommiza_app/view/activity/AddItemActivity.java
@@ -0,0 +1,32 @@
+package com.automatodev.e_conommiza_app.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.automatodev.e_conommiza_app.R;
+
+public class AddItemActivity extends AppCompatActivity {
+    public static boolean status;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_item);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        status = true;
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();;
+        status = false;
+    }
+}
